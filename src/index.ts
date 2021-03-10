7dd1fe7c9db94f65dce78d07b1c4115d124bb4d2
FuSun <332553703@qq.com>
2021-03-10 10:46:34 +0800

    feat: 遍历

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,36 @@
 import * as fs from "fs";
+import * as Path from "path";
 
-const f = fs.readdirSync(process.cwd());
-console.log(f);
+const ignoreList: string[] = [".git", "node_modules", ".vscode", ".idea"];
+interface FileDirHashTree {
+  hash: string;
+  children: FileDirHashTree[];
+}
+
+const deep = (path: string) => {
+  if (!fs.lstatSync(path).isDirectory()) {
+    return {
+      path,
+      hash: "",
+      children: [],
+    };
+  }
+
+  const children: FileDirHashTree[] = [];
+
+  const list = fs.readdirSync(path);
+  for (const childDir of list) {
+    if (ignoreList.indexOf(childDir) != -1) {
+      continue;
+    }
+    children.push(deep(Path.join(path, childDir)));
+  }
+  return {
+    path,
+    hash: "",
+    children,
+  };
+};
+
+const tree = deep(process.cwd());
+console.log(JSON.stringify(tree, null, 2));
