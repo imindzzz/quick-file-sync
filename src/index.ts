d0c4d0ad301fa1c6cb7bbba79fa64af9eb0fd142
FuSun <332553703@qq.com>
2021-06-18 09:57:07 +0800

    chore: oss

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
-import * as path from "path";
-import { buildTree } from "./FileTree";
+import * as OSS from "ali-oss";
 
-(async () => {
-  console.time("buildTree");
-  console.log("buildTree start");
-  await buildTree(path.join(process.cwd(), "node_modules"));
-  console.log("buildTree end");
-  console.timeEnd("buildTree");
-})();
+let client = new OSS({
+  region: "oss-cn-beijing",
+  accessKeyId: "LTAI5tPESUoga9rBa4q2AMEB",
+  accessKeySecret: "zGKFbyVQay0dPMAchbGdWvMLsvpj4g",
+  // bucket: "kq-static",
+});
+
+async function getBucketInfo() {
+  const result = await client.getBucketInfo("kq-static");
+  // 获取存储空间所在的地域。
+  console.log(result.bucket.Location);
+  // 获取存储空间的名称。
+  console.log(result.bucket.Name);
+  // 获取存储空间的拥有者ID。
+  console.log(result.bucket.Owner.ID);
+  // 获取存储空间的拥有者名称，目前和拥有者ID一致。
+  console.log(result.bucket.Owner.DisplayName);
+  // 获取存储空间的创建时间。
+  console.log(result.bucket.CreationDate);
+  // 获取存储空间的存储类型。
+  console.log(result.bucket.StorageClass);
+  // 获取存储空间的版本控制状态。
+  console.log(result.bucket.Versioning);
+}
+
+getBucketInfo();
